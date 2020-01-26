2b41f0e55161496ed9182a286d6f1d181bf498b3
Hector-1911 <fontas.chrome@gmail.com>
2020-01-26 20:15:47 +0300

    12313

--- js/main.js
+++ js/main.js
@@ -140,6 +140,27 @@ dotRight.addEventListener("click", function(evt) {
     redDotRight.classList.remove('display-off');
 });
 
+var loginInButton = document.querySelector('.panel-login-in');
+var loginOutButton = document.querySelector('.user-exit');
+var userLogin = document.querySelector('.middle-panel-login');
+var userInfo = document.querySelector('.middle-panel-login-area');
+
+loginInButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    userLogin.classList.remove('display-on');
+    userLogin.classList.add('display-off');
+    userInfo.classList.add('display-on');
+    userInfo.classList.remove('display-off');
+});
+
+loginOutButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    userInfo.classList.remove('display-on');
+    userInfo.classList.add('display-off');
+    userLogin.classList.add('display-on');
+    userLogin.classList.remove('display-off');
+});
+
 
 
 
